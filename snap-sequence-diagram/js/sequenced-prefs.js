4f789e6591bc975eb89b0ca4fb078bb470bf0e66
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2016-07-06 17:04:59 +0530

    Adding backbone and snap svg based sequence diagram editor POC.

---
+++ snap-sequence-diagram/js/sequenced-prefs.js
@@ -0,0 +1,40 @@
+/**
+ * SequenceD-Prefs Module extension.
+ *
+ * Initialize preferences.
+ */
+var SequenceD = (function (sequenced) {
+    var prefs = sequenced.prefs || {},
+        lifeLinePrefs = prefs.lifeline || {},
+        paperPrefs = prefs.paper || {};
+
+    // set css selector for paper
+    paperPrefs.selector = "#svgArea ";
+
+    // prefs for lifeline view
+    lifeLinePrefs.class = "lifeline";
+    // rectangle prefs
+    lifeLinePrefs.rect = lifeLinePrefs.rect || {};
+    lifeLinePrefs.rect.width = 200;
+    lifeLinePrefs.rect.height = 75;
+    lifeLinePrefs.rect.roundX = 20;
+    lifeLinePrefs.rect.roundY = 20;
+    lifeLinePrefs.rect.class = "lifeline-rect";
+    // line prefs
+    lifeLinePrefs.line = lifeLinePrefs.line || {};
+    lifeLinePrefs.line.height = 800;
+    lifeLinePrefs.line.class = "lifeline-line";
+    // text prefs
+    lifeLinePrefs.text = lifeLinePrefs.text || {};
+    lifeLinePrefs.text.class = "lifeline-title";
+
+    // set pref categories
+    prefs.lifeline = lifeLinePrefs;
+    prefs.paper = paperPrefs;
+
+    // set global prefs
+    sequenced.prefs = prefs;
+
+    return sequenced;
+
+}(SequenceD || {}));
