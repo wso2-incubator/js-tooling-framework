65fa785bef588232b1008f82edbc53b66606e6f3
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2016-07-12 10:54:48 +0530

    Refactoring & add support for passing parent element in args

--- d3-backbone-based-sequence-editor/js/d3-drawing.js
+++ d3-backbone-based-sequence-editor/js/d3-drawing.js
@@ -2,21 +2,24 @@ var d3_draw = (function (d3_draw) {
 
     var d3Ref = undefined;
 
-    var circle = function(cx,cy,r){
-        return d3Ref.append("circle")
+    var circle = function(cx, cy, r, parent){
+        parent = parent || d3Ref;
+        return parent.append("circle")
             .attr("cx",cx)
             .attr("cy",cy)
             .attr("r",r);
     };
 
-    var circleOnPoint = function(point,r){
-        return d3Ref.circle(point.getX(), point.getY(), r);
+    var circleOnPoint = function(point, r, parent){
+        parent = parent || d3Ref;
+        return parent.circle(point.getX(), point.getY(), r);
     };
 
-    var rect = function(x,y,width,height,rx,ry){
+    var rect = function(x, y, width, height, rx, ry, parent){
+        parent = parent || d3Ref;
         rx = rx || 0;
         ry = ry || 0;
-        return d3Ref.append("rect")
+        return parent.append("rect")
             .attr("x",x)
             .attr("y",y)
             .attr("width",width)
@@ -25,40 +28,34 @@ var d3_draw = (function (d3_draw) {
             .attr("ry",ry);
     };
 
-    /**
-     * Draws a rectangle which is centered to a given point in the paper.
-     *
-     * @param center GraphicsCore.Models.Point object which maps the center point.
-     * @param width  Width of the
-     * @param height
-     * @param rx
-     * @param ry
-     */
-    var centeredRect = function(center, width, height, rx, ry){
+    var centeredRect = function(center, width, height, rx, ry, parent){
+        parent = parent || d3Ref;
         rx = rx || 0;
         ry = ry || 0;
-        return d3Ref.rect(center.getX() - width/2, center.getY() - height/2, width, height, rx, ry);
+        return parent.rect(center.getX() - width/2, center.getY() - height/2, width, height, rx, ry, parent);
     };
 
-
-    var line = function(x1, y1, x2, y2){
-       return d3Ref.append("line")
+    var line = function(x1, y1, x2, y2, parent){
+       parent = parent || d3Ref;
+       return parent.append("line")
            .attr("x1",x1)
            .attr("y1",y1)
            .attr("x2",x2)
            .attr("y2",y2);
     };
 
-    var horizontalLine = function(start, height){
-        return d3Ref.line(start.getX(), start.getY(), start.getX(), start.getY() + height);
+    var verticalLine = function(start, height, parent){
+        parent = parent || d3Ref;
+        return parent.line(start.getX(), start.getY(), start.getX(), start.getY() + height, parent);
     };
 
     var editableText = function(x, y, text){
 
     };
 
-    var textElement = function(x, y, textContent){
-        return d3Ref.append("text")
+    var textElement = function(x, y, textContent, parent){
+        parent = parent || d3Ref;
+        return parent.append("text")
             .attr("x",x)
             .attr("y",y)
             .text(function () {
@@ -66,19 +63,29 @@ var d3_draw = (function (d3_draw) {
             });
     };
 
-    var centeredText = function(center, textContent){
-        return d3Ref.textElement(center.getX(), center.getY(), textContent).attr('text-anchor', 'middle');
+    var centeredText = function(center, textContent, parent){
+        parent = parent || d3Ref;
+        return parent.textElement(center.getX(), center.getY(), textContent, parent).attr('text-anchor', 'middle');
     };
 
     var lifeLine = function (center, title, prefs) {
-        var rect = d3Ref.centeredRect(center, prefs.rect.width, prefs.rect.height).classed(prefs.rect.class, true);
-        var line = d3Ref.horizontalLine(center, prefs.line.height).classed(prefs.line.class, true);
-        var text = d3Ref.centeredText(center, title).classed(prefs.text.class, true);
-        var group = d3Ref.group([rect, line, text]);
+        var group = d3Ref.group();
+        var rect = d3Ref.centeredRect(center, prefs.rect.width, prefs.rect.height, 10, 10, group)
+                            .classed(prefs.rect.class, true);
+        var line = d3Ref.verticalLine(center, prefs.line.height, group)
+                            .classed(prefs.line.class, true);
+        var text = d3Ref.centeredText(center, title, group)
+                            .classed(prefs.text.class, true);
         return group;
     };
 
-    var group = function(elements){
+    var group = function(parent){
+        parent = parent || d3Ref;
+        return parent.append("g");
+    };
+
+    // FIXME: refactor to use native window methods
+    var regroup = function(elements){
         var g = d3Ref.append("g");
         var $g = jQuery(g.node());
         elements.forEach(function(element){
@@ -96,7 +103,7 @@ var d3_draw = (function (d3_draw) {
         d3Ref.__proto__.centeredRect = centeredRect;
         d3Ref.__proto__.rect = rect;
         d3Ref.__proto__.line = line;
-        d3Ref.__proto__.horizontalLine = horizontalLine;
+        d3Ref.__proto__.verticalLine = verticalLine;
         d3Ref.__proto__.editableText = editableText;
         d3Ref.__proto__.centeredText = centeredText;
         d3Ref.__proto__.textElement = textElement;
