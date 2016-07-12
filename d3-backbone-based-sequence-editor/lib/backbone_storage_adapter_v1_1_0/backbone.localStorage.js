aa3842cfe1dbc5cfd5defd576f5ffd55bb3b6318
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2016-07-12 10:52:09 +0530

    Moving third party libs to lib folder.

--- d3-backbone-based-sequence-editor/lib/backbone_storage_adapter_v1_1_0/backbone.localStorage.js
+++ d3-backbone-based-sequence-editor/lib/backbone_storage_adapter_v1_1_0/backbone.localStorage.js
@@ -7,7 +7,7 @@
 (function (root, factory) {
    if (typeof define === "function" && define.amd) {
       // AMD. Register as an anonymous module.
-      define(["underscore","backbone"], function(_, Backbone) {
+      define(["../underscore_v1_8_3/underscore","backbone"], function(_, Backbone) {
         // Use global variables if the locals are undefined.
         return factory(_ || root._, Backbone || root.Backbone);
       });
