2523dc56c6f8f4e8570c347e6e074b8b04c44ef5
susinda <susinda@wso2.com>
2016-08-30 14:22:04 +0530

    positioning and z-index issue fixed

--- sequence-editor/js/diagram/diagram-view.js
+++ sequence-editor/js/diagram/diagram-view.js
@@ -298,7 +298,7 @@ var Diagrams = (function (diagrams){
 		position.x = ui.offset.left - $(this).offset().left;
 		position.y = ui.offset.top - $(this).offset().top;
 		console.log(position);
-                var lifeline4 = createLifeLine("LifeLine4",createPoint(position.x, position.y));
+                var lifeline4 = createLifeLine("LifeLine4",createPoint(position.x, 50));
                 diagram.addElement(lifeline4, lifeLineOptions);
 	},
 
