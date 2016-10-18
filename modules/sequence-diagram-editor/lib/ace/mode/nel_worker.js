0899b53ee01ce8358502ed55911cb55cd38f2560
Himasha Guruge <himashag@wso2.com>
2016-10-18 15:42:04 +0530

    Removing extra themes/modes from ace library

--- modules/sequence-diagram-editor/lib/ace/mode/nel_worker.js
+++ modules/sequence-diagram-editor/lib/ace/mode/nel_worker.js
@@ -28,25 +28,96 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-define(function (require, exports, module) {
+define(function(require, exports, module) {
     "use strict";
 
     var oop = require("../lib/oop");
     var Mirror = require("../worker/mirror").Mirror;
     var antlr4 = require("../antlr4/index");
-    var nel = require("../nel-gen/index");
 
-    var NelWorker = exports.NelWorker = function (sender) {
+  // var antlr4 = require("../antlr4/index");
+    var nel = require("../nel-gen/index");
+   // var lint = require("./javascript/jshint").JSHINT;
+
+    function startRegex(arr) {
+        return RegExp("^(" + arr.join("|") + ")");
+    }
+
+    var disabledWarningsRe = startRegex([
+        "Bad for in variable '(.+)'.",
+        'Missing "use strict"'
+    ]);
+    var errorsRe = startRegex([
+        "Unexpected",
+        "Expected ",
+        "Confusing (plus|minus)",
+        "\\{a\\} unterminated regular expression",
+        "Unclosed ",
+        "Unmatched ",
+        "Unbegun comment",
+        "Bad invocation",
+        "Missing space after",
+        "Missing operator at"
+    ]);
+    var infoRe = startRegex([
+        "Expected an assignment",
+        "Bad escapement of EOL",
+        "Unexpected comma",
+        "Unexpected space",
+        "Missing radix parameter.",
+        "A leading decimal point can",
+        "\\['{a}'\\] is better written in dot notation.",
+        "'{a}' used out of scope"
+    ]);
+
+    var NelWorker = exports.NelWorker = function(sender) {
         Mirror.call(this, sender);
         this.setTimeout(500);
+        this.setOptions();
     };
 
     oop.inherits(NelWorker, Mirror);
 
-    (function () {
+    (function() {
+        this.setOptions = function(options) {
+            this.options = options || {
+                    // undef: true,
+                    // unused: true,
+                    esnext: true,
+                    moz: true,
+                    devel: true,
+                    browser: true,
+                    node: true,
+                    laxcomma: true,
+                    laxbreak: true,
+                    lastsemic: true,
+                    onevar: false,
+                    passfail: false,
+                    maxerr: 100,
+                    expr: true,
+                    multistr: true,
+                    globalstrict: true
+                };
+            this.doc.getValue() && this.deferredUpdate.schedule(100);
+        };
 
+        this.changeOptions = function(newOptions) {
+            oop.mixin(this.options, newOptions);
+            this.doc.getValue() && this.deferredUpdate.schedule(100);
+        };
 
-        this.onUpdate = function () {
+        this.isValidJS = function(str) {
+            try {
+                // evaluated code can only create variables in this function
+                eval("throw 0;" + str);
+            } catch(e) {
+                if (e === 0)
+                    return true;
+            }
+            return false
+        };
+
+        this.onUpdate = function() {
             var value = this.doc.getValue();
             var errors = [];
             var results = validate(value);
@@ -54,10 +125,10 @@ define(function (require, exports, module) {
             this.sender.emit("annotate", results);
 
         };
-        var validate = function (input) {
+        var validate = function(input) {
 
 
-            var parseTreeForAce = function (input) {
+            var parseTreeForAce = function(input) {
                 var stream = new antlr4.InputStream(input);
                 var lexer = new nel.WUMLLexer(stream);
                 var tokens = new antlr4.CommonTokenStream(lexer);
@@ -65,13 +136,15 @@ define(function (require, exports, module) {
                 var annotations = [];
                 var listener = new AnnotatingErrorListener(annotations);
                 parser.removeErrorListeners();
+                //lexer.removeErrorListeners();
+                //lexer.addErrorListener(listener);
                 parser.addErrorListener(listener);
                 parser.buildParseTrees = true;
                 parser.sourceFile();
                 return annotations;
             };
 
-            var AnnotatingErrorListener = function (annotations) {
+            var AnnotatingErrorListener = function(annotations) {
                 antlr4.error.ErrorListener.call(this);
                 this.annotations = annotations;
                 return this;
@@ -80,7 +153,7 @@ define(function (require, exports, module) {
             AnnotatingErrorListener.prototype = Object.create(antlr4.error.ErrorListener.prototype);
             AnnotatingErrorListener.prototype.constructor = AnnotatingErrorListener;
 
-            AnnotatingErrorListener.prototype.syntaxError = function (recognizer, offendingSymbol, line, column, msg, e) {
+            AnnotatingErrorListener.prototype.syntaxError = function(recognizer, offendingSymbol, line, column, msg, e) {
                 this.annotations.push({
                     row: line - 1,
                     column: column,
@@ -92,6 +165,7 @@ define(function (require, exports, module) {
             var finalResult = parseTreeForAce(input);
             return finalResult;
 
+            //  return [ { row: 0, column: 0, text: "MyMode says Hello!", type: "error" } ];
         };
 
     }).call(NelWorker.prototype);
