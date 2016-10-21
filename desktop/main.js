bdbfd68dc14301d8a065230d850b6e1bc4769d6c
Kavith Thiranga Lokuhewage <kaviththiranga@gmail.com>
2016-10-21 20:50:14 +0530

    Adjusting screen resolution.

--- desktop/main.js
+++ desktop/main.js
@@ -24,7 +24,13 @@ let mainWindow;
 
 function createWindow () {
 	// Create the browser window
-	mainWindow = new BrowserWindow({width: 800, height: 600, })
+	mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		minWidth: 1200,
+		minHeight: 800,
+		fullscreen: true
+	})
 	
 	// Load the index.html of the application
 	mainWindow.loadURL(`file://${__dirname}/modules/sequence-diagram-editor/index.html`)
